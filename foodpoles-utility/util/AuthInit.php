4633fe0377d9c3c63c5da9e5b590f2118f8691e4
itsamiksha <itsamiksha@gmail.com>
2014-12-28 10:38:10 +0530

    php first

--- foodpoles-utility/util/AuthInit.php
+++ foodpoles-utility/util/AuthInit.php
@@ -8,7 +8,7 @@
 
 session_start();
 
-require_once'/../vendor/google-api-php-client-master/autoload.php';
+require_once __DIR__.'/../vendor/google-api-php-client-master/autoload.php';
 require_once'GoogleAuth.php';
 
 
